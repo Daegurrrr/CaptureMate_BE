fc46e57ba7fa5ddb0acb67c999a8974ece442eb7
jieunee32 <hsilver0302@gmail.com>
2026-05-26 01:04:28 +0900

    feat: :새 학습 모델 DB 저장 엔드포인트추가

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     version="0.1.0"
 )
 
-app.include_router(ai.router)  
+app.include_router(ai.router, prefix = "/ai")  
 app.include_router(screenshot.router, prefix="/screenshots")
 app.include_router(place.router)
 app.include_router(auth.router)
